69a60857d8f703170c2d9e08dd6706d7dedcd18e
panesofglass <ryan.riley@panesofglass.org>
2015-06-04 10:28:47 -0500

    Add test projects

---
+++ tests/Arachne.Core.Tests/Prelude.fs
@@ -0,0 +1,13 @@
+﻿[<AutoOpen>]
+module Arachne.Core.Tests.Prelude
+
+open Swensen.Unquote.Assertions
+
+(* Helpers *)
+
+let inline (=?) a b = test <@ a = b @>
+
+let roundTrip<'a when 'a: equality> (iso: ('a -> string) * (string -> 'a)) =
+    List.iter (fun (a, s) ->
+        (fst iso) a =? s
+        (snd iso) s =? a)
