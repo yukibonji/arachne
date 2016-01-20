d97f1f740a004b4ec68e4065173d93268c8c3d96
Max Malook <max.malook@hotelplan.com>
2016-01-20 09:50:46 +0100

    add test to ensure roundtrip works in locales with other decimal separator, like 'de'

--- tests/Arachne.Core.Tests/Prelude.fs
+++ tests/Arachne.Core.Tests/Prelude.fs
@@ -2,12 +2,26 @@
 module Arachne.Core.Tests.Prelude
 
 open Swensen.Unquote.Assertions
+open System.Globalization
+open System.Threading
 
 (* Helpers *)
 
 let inline (=?) a b = test <@ a = b @>
 
+let private culturesToTest = [
+    CultureInfo("en")
+    CultureInfo("de") ]
+
+let private useCulture c =
+    let current = Thread.CurrentThread.CurrentCulture
+    Thread.CurrentThread.CurrentCulture <- c
+    { new System.IDisposable with
+          member x.Dispose() = Thread.CurrentThread.CurrentCulture <- current }
+
 let roundTrip<'a when 'a: equality> (iso: ('a -> string) * (string -> 'a)) =
-    List.iter (fun (a, s) ->
-        (fst iso) a =? s
-        (snd iso) s =? a)
+        List.collect (fun p -> List.map (fun x -> x, p) culturesToTest)
+     >> List.iter (fun (c, (a, s)) ->
+            use __ = useCulture c
+            (fst iso) a =? s
+            (snd iso) s =? a)
